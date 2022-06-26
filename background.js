830aeb9d1e9ad36d54c504bc9ffb6ee46d35e7a3
vikash <megasoft909@gmail.com>
2022-06-26 22:33:24 +0530

    updates

--- background.js
+++ background.js
@@ -5,6 +5,37 @@ chrome.downloads.onCreated.addListener(
         console.log('Downlod-file-data', data);
         fetch('https://jsonplaceholder.typicode.com/todos/1')
             .then(response => response.json())
-            .then(json => console.log(json))
+            .then(json => console.log('json-data', json))
     }
 )
+
+
+
+
+
+
+
+// let isAllow = false;
+
+
+// chrome.storage.sync.set({ key: false });
+
+// chrome.storage.sync.get(['key'], function (result) {
+//     // console.log('Value currently is ' + result.key);
+//     isAllow = result.key;
+// });
+
+
+// if (isAllow) {
+
+// chrome.downloads.onCreated.addListener(
+//     (data) => {
+
+//         console.log('Downlod-file-data', data);
+//         fetch('https://jsonplaceholder.typicode.com/todos/1')
+//             .then(response => response.json())
+//             .then(json => console.log(json))
+//     }
+// )
+// }
+
