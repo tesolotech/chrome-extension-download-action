47438c16e56b719652650bcb3c74549dc3768211
vikash <megasoft909@gmail.com>
2022-07-03 17:39:58 +0530

    worked on regex match

--- background.js
+++ background.js
@@ -1,3 +1,23 @@
+// Helpful detaild to generate and verify url patter with regex
+
+// # To generate regex of url pattern
+// ------ https://regex-generator.olafneumann.org/?sampleText=*.sharepoint.com&flags=i&onlyPatterns=false&matchWholeLine=true&selection=
+
+// To do tesing only 
+// ------ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
+
+
+// Testing url
+// --- -- https://unsplash.com/photos/FLRgERkP1j0
+
+// related URL
+// ------ https://www.regextester.com/20
+
+// URL pROPERTIES
+// ------ https://www.techtarget.com/searchnetworking/definition/URL
+
+
+
 
 const urlPatternRegex = [
     "*unsplash.com",
@@ -6,10 +26,19 @@ const urlPatternRegex = [
     "*flipcart.com*"
 ];
 
+const domainRegex = [
+    '^\*unsplash\.com$'
+]
+
+// { "*.sharepoint.com": '/^\*\.sharepoint\.com$/i' },
+//     { "*unsplash.com": '^\*unsplash\.com$' },
+//     { "*flipcart.com*": "^\*flipcart\.com\*$" },
+
+
 function isURLMatchPatter(url) {
     let result = false;
-    for (let i = 0; i < urlPatternRegex.length; i++) {
-        const regex = '/' + urlPatternRegex[i] + '/'
+    for (let i = 0; i < domainRegex.length; i++) {
+        const regex = '/' + domainRegex[i] + '/'
         const urlPattern = new RegExp(regex)
         result = urlPattern.test(url);
         if (result) {
